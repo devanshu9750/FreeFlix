cb211675edaa992ee7fda023c91a1a4fabab9518
devanshu9750 <devg9750@gmail.com>
2020-11-01 18:34:26 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/FreeFlix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.FreeFlix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:FreeFlix/screens/home/Home.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    home: App(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class App extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Text("Error !!"),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Home();
+        }
+        return Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+    );
+  }
+}
