515bcd3f50c23485cbe02374404a1cd426cbe416
Deep Mehta <deepmehta8855@gmail.com>
2020-11-12 01:34:07 +0530

    report

--- lib/screens/drawer/Report.dart
+++ lib/screens/drawer/Report.dart
@@ -1,9 +1,10 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 import 'package:velocity_x/velocity_x.dart';
 
 class Report extends StatelessWidget {
-  String prb="";
+  String prb = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,24 +12,50 @@ class Report extends StatelessWidget {
         title: "Report".text.make(),
       ),
       body: VStack([
-        
-         TextFormField(
-              onChanged: (value) {
-                prb = value;
-              },
-              maxLines: 5,
-              scrollPhysics: ScrollPhysics(),
-              decoration: InputDecoration(
-                  // labelText: "Report Your Problem ",
-                  hintText: "Eg.  Anime- (Dubbed)My hero acaademia Season 2  episode 1 ",
-                  contentPadding: EdgeInsets.all(10),
-                  enabledBorder: OutlineInputBorder(
-                    gapPadding: 20,
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)))),
-            ).p(20),
+        TextFormField(
+          onChanged: (value) {
+            prb = value;
+          },
+          maxLines: 10,
+          scrollPhysics: ScrollPhysics(),
+          decoration: InputDecoration(
+              labelText: "Problems",
+              labelStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+              hintText:
+                  "Eg.  Anime- (Dubbed) My hero acaademia Season 2  episode 1 ",
+              contentPadding: EdgeInsets.all(10),
+              enabledBorder: OutlineInputBorder(
+                gapPadding: 20,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)))),
+        ).p(20),
+        Center(
+          child: RaisedButton(
+              color: Colors.black45,
+              elevation: 20,
+              padding: EdgeInsets.only(left: 60, right: 60),
+              child: "Submit".text.white.make(),
+              splashColor: Colors.red,
+              onPressed: () {
+                 const _chars =
+                      'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+                  Random _rnd = Random();
+
+                  String getRandomString(int length) =>
+                      String.fromCharCodes(Iterable.generate(
+                          length,
+                          (_) =>
+                              _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
+                
+                FirebaseDatabase.instance.reference().child("report").child(getRandomString(20)).update({"problems":prb});
+                Navigator.of(context).pop();
+              }),
+        )
       ]).scrollVertical(),
     );
   }
