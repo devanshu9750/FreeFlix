c13fe51fadab2d6c3e06731c98418bd2b766d8b6
devanshu9750 <devg9750@gmail.com>
2021-03-22 15:50:39 +0530

    Version 2

--- android/app/src/main/kotlin/com/example/FreeFlix/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.FreeFlix
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/data/SearchData.dart
+++
@@ -1,5 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class SearchData {
-  static List<QueryDocumentSnapshot> data;
-}
